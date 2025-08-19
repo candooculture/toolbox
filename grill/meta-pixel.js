f9355d7a0bf013fbfd186c631288adc1d5689d9d
candooculture <james@candooculture.com>
2025-08-19 12:45:06 +1000

    Add files via upload

---
+++ grill/meta-pixel.js
@@ -0,0 +1,9 @@
+<!-- contents of meta-pixel.js (yes, it’s plain JS) -->
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1120593203336237');
+fbq('track', 'PageView');
